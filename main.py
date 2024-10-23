77f2b5e7608437c2414ec023901ad29ab5719f0b
Latrell Clark <clarklat000@warren.k12.in.us>
2024-10-23 08:16:51 -0400

    int

---
+++ main.py
@@ -0,0 +1,16 @@
+age = 15
+
+
+
+if age >= 5 :
+    print("You should play with toys.")
+
+if age >= 5 and age <= 12:
+    print("You can play outdoor game.")
+
+if age >= 13 and age <= 17:
+    print("You might enjoy video games.")
+if age >= 18:
+    print("You can try hiking or traveling")
+
+
